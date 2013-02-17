9d557df7352688927f1932647b053e54dd728308
redblast71 <redblast71@Steph-laptop>
2013-02-16 19:01:18 -0600

    Working on so many things, so so many.

---
+++ engine/Callback.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package engine;
+
+/**
+ *
+ * @author redblast71
+ */
+public interface Callback<T> {
+    public T call();
+}
